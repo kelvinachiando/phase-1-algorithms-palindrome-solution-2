befc02c209a07865b2c99d2b400e40e54060dd25
kelvinachiando <kelvinostin22gmail.com>
2022-06-17 07:17:59 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++){
+    const j= word.length-1-i
+    const startChar = word[i]
+    const endChar = word[j]
+    if (startChar !== endChar)return false;
+    return true;
+  }
 }
 
 /* 
   Add your pseudocode here
+  I need to make an isPalindrome function that returns either true or false. 
+  When the input string is the same forwards and backwards, I should return true. 
+  That means if the first letter is the same as the last letter, 
+  and the second letter is the same as the second to last letter, 
+  and so on (until the middle of the word), the function should return true.
+
+  iterate from the beginning to the middle
+    check each letter to the correspinding letter from the end
+    if any letter don't match, return false.
+  return true
+
+  r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
 */
 
 /*
